835dd415e5315a1bf848152cb204d198b566cdfa
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-09-20 11:58:35 +0200

    testing out folder

---
+++ out/_next/static/JKs0p-yeVmnrgvLtwFWiS/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-e732c5b63773e86730fa.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-4caa4d89f4eb1c8ddace.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
