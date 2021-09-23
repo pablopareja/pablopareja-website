a1b2189961e5f3fa2d63926e7c260e4e2a5ed375
pablopareja <pablopareja@users.noreply.github.com>
2021-09-23 14:54:58 +0000

    deploy: c93382e39b484c8a3889e32ad4a042242102ee08

--- _next/static/kBPm2uDiG1yp_SzKTsU16/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-7d377a3383c2b51ced51.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e9a92067cbdd0255534c.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
