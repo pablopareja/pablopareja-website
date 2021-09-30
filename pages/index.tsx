01d1d1ca1e65559a1379be65410f861b12c82f80
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-09-30 17:20:22 +0200

    first version of nav menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,14 +38,45 @@ const Home: React.FC = () => {
           >
             <source src="/videos/the_sea_teaser.mp4" type="video/mp4" />
           </video>
-          <div className="relative">
+          <nav className="relative flex justify-between w-full text-xs text-white">
             <Link href="/">
               {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
               <a>
                 <img src="/images/pablo_pareja_logo.svg" alt="Pablo Pareja" />
               </a>
             </Link>
-          </div>
+            {showSecretSections && (
+              <div>
+                <Link href="/#media">
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a className="leading-4 ml-14" style={{ letterSpacing: '0.6px' }}>
+                    MEDIA
+                  </a>
+                </Link>
+                <Link href="/#news">
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a className="leading-4 ml-14" style={{ letterSpacing: '0.6px' }}>
+                    NEWS
+                  </a>
+                </Link>
+                <Link href="/#events">
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a className="leading-4 ml-14" style={{ letterSpacing: '0.6px' }}>
+                    EVENTS
+                  </a>
+                </Link>
+                <Link href="/#contact">
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a
+                    className="px-10 py-2 leading-4 border border-white ml-14"
+                    style={{ letterSpacing: '0.6px' }}
+                  >
+                    CONTACT
+                  </a>
+                </Link>
+              </div>
+            )}
+          </nav>
 
           {!showSecretSections && (
             <motion.div
