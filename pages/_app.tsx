380e18e045dc09bf834bcb7fdf9324eb7dbe89eb
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2021-10-20 20:12:35 +0200

    seo fix try

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { DefaultSeo, SocialProfileJsonLd } from 'next-seo';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { OverlayProvider } from '@react-aria/overlays';
@@ -9,6 +10,8 @@ import store from 'store';
 import 'styles/globals.css';
 import { Desktop, MediaContextProvider, Mobile } from 'utils/responsive';
 
+import SEO from '../next-seo.config';
+
 const queryClient = new QueryClient();
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
@@ -16,6 +19,16 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
         <OverlayProvider>
+          <DefaultSeo {...SEO} />
+          <SocialProfileJsonLd
+            type="Person"
+            name="Pablo Pareja"
+            url="https://www.pablopareja.com"
+            sameAs={[
+              'http://www.facebook.com/pabloparejamusic',
+              'http://instagram.com/pabloparejatobes',
+            ]}
+          />
           <MediaContextProvider>
             <Mobile>
               <Component {...pageProps} />
